0f4058ababed034697ed7790d5f793d728fb764b
MizzleNa <mizzleaaa@gmail.com>
2022-05-23 13:05:00 +0900

    ⚡️ opencv를 이용한 dtype 자동 판별 기능 추가

--- library/utils/io.py
+++ library/utils/io.py
@@ -1,5 +1,42 @@
 from library.utils.header import *
 
+def cv_load_image(options):
+    '''
+    options = {
+        "file_name": {file_name},
+    }
+    '''
+    file_name = options["file_name"]
+    
+    image_array = np.fromfile(file_name, np.uint8)
+    result = cv2.imdecode(image_array, -1)
+
+    print(f"load image dtype={result.dtype} shape={result.shape}")
+
+    return result
+
+def cv_save_image(data, options):
+    '''
+    options = {
+        "file_name": {file_name},
+    }
+    '''
+    file_name = options["file_name"]
+    dtype = options["dtype"]
+    start_pixel = options["start_pixel"]
+    end_pixel = options["end_pixel"]
+
+    extension = os.path.splitext(file_name)[1]
+    data = np.asarray(
+        np.clip(data, start_pixel, end_pixel), dtype=dtype
+    )
+    result, encoded_img = cv2.imencode(extension, data)
+
+    if result: 
+        with open(file_name, mode="w+b") as f: 
+            encoded_img.tofile(f)
+    
+    
 def load_image(options):
     '''
     options = {
